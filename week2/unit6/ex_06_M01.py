8ae2bedb3c1284a696b69c9b49246e538d634654
Jim Strachey <Jim@Jims-MacBook-Pro.local>
2017-11-22 13:46:37 +0000

    Jems updates

--- week2/unit6/ex_06_M01.py
+++ week2/unit6/ex_06_M01.py
@@ -5,8 +5,8 @@ print("------------------------     M01     -------------------------")
 Cnum=""
 #while type != Cnum.isnumeric():
 Cnum = input("Please enter a number in Celcius:- ")
-Fnum = ((int(Cnum)*9)/5)+32
+Fnum = ((float(Cnum)*9)/5)+32
 print("The number in Celcius was {} and converting it to Fahrenheit its {}".format(Cnum,Fnum))
 Fnum = input("Please enter a number in Fahrenheit:- ")
-Cnum = (int(Fnum)-32)*(5/9)
+Cnum = (float(Fnum)-32)*(5/9)
 print("The number in Fahrenheit was {} and converting it to Celcius its {}".format(Fnum,Cnum))
--- week2/unit6/ex_08_M02.py
+++ week2/unit6/ex_08_M02.py
@@ -1,5 +1,7 @@
 # Import libraries
 import math
+import locale
+locale.setlocale(locale.LC_ALL, '')
 #
 #
 #Define functions
@@ -22,21 +24,19 @@ def intInput(prompt,errMess):
             print("\t{}\n".format(errMess))
 #
 # Entering a single letter
-def letInput(prompt,errMess):
+def letInput(prompt,valChars,errMess):
     x = True
     while x:
         Str = input(prompt)
-        if Str.upper() == "P":
-            return 5 # Return the offset to the correct price in pricelist[]
-        elif Str.upper() == "O":
-            return 0 # Return the offset to the correct price in pricelist[]
+        if len(Str) == 1 and Str.upper() in valChars:
+            return Str.upper() # Return the offset to the correct price in pricelist[]
         else:
             print("\t{}\n".format(errMess))
 #
 # Prepare price list
-#           (     off peak    )  (      peak       )
-#            C,  T,  St, Se, A,  C,  T,  St, Se, A
-priceList = [5,  7,  7,  7,  8,  7,  9,  9,  9,  10]
+#                 (               off peak                  )  (                  peak                  )
+#                  Child,   Teen,    Stud,    Senior,  Adult,   Child,   Teen,    Stud,    Senior,  Adult
+priceList     = [  5,       7,       7,       7.53,    8,       7,       9,       9,       9.12,    10]
 #
 # Initialise list for count of each visitor type.
 #                   C,  T,  St, Se, A
@@ -45,30 +45,33 @@ visitorCountList = [0,  0,  0,  0,  0]
 print("\n--------------------------------------------------------------")
 print("------------------------   Ex08-M02   ------------------------")
 print("\n\n")
-print("Hi, Welcome to the virtual cinema, we're showing python of the world")
-print("\nWe have two price ranges based on date and status")
-print("-----------------------------------------------------------------------------------------")
-print("|\t\t\t|\t     Off Peak\t\t|\t\tPeak\t\t|")
-print("|\t\t\t|\t(Monday to Thusday)\t|\t (Friday to Sunday)\t|")
-print("|-----------------------|-------------------------------|-------------------------------|")
-print("|Child\t\t\t|\t\t£{:.2f}\t\t|\t\t£{:.2f}\t\t|".format(priceList[0],priceList[5]))
-print("|(12 years and under)\t|\t\t\t\t|\t\t\t\t|")
-print("|-----------------------|-------------------------------|-------------------------------|")
-print("|Teen\t\t\t|\t\t£{:.2f}\t\t|\t\t£{:.2f}\t\t|".format(priceList[1],priceList[6]))
-print("|(Ages 13 to 17)\t|\t\t\t\t|\t\t\t\t|")
-print("|-----------------------|-------------------------------|-------------------------------|")
-print("|Student\t\t|\t\t£{:.2f}\t\t|\t\t£{:.2f}\t\t|".format(priceList[2],priceList[7]))
-print("|(Student ID)\t\t|\t\t\t\t|\t\t\t\t|")
-print("|-----------------------|-------------------------------|-------------------------------|")
-print("|Senior\t\t\t|\t\t£{:.2f}\t\t|\t\t£{:.2f}\t\t|".format(priceList[3],priceList[8]))
-print("|(Ages 60+)\t\t|\t\t\t\t|\t\t\t\t|")
-print("|-----------------------|-------------------------------|-------------------------------|")
-print("|Adult\t\t\t|\t\t£{:.2f}\t\t|\t\t£{:.2f}\t\t|".format(priceList[4],priceList[9]))
-print("|(Ages 18+)\t\t|\t\t\t\t|\t\t\t\t|")
-print("-----------------------------------------------------------------------------------------")
-print("")
+print("Hi, Welcome to the virtual cinema, we're showing 'Pythons of the World'")
+print("Here is our price list")
+print("{0:-<28}{0:-<28}{0:-<29}".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format(" ","Off Peak","Peak"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format(" ","(Monday to Thursday)","(Friday to Sunday)"))
+print("|{0:-<27}|{0:-<27}|{0:-<27}|".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("Child",locale.currency(priceList[0]),locale.currency(priceList[5])))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("(12 years and under)"," "," "))
+print("|{0:-<27}|{0:-<27}|{0:-<27}|".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("Teen",locale.currency(priceList[1]),locale.currency(priceList[6])))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("(Ages 13 to 17)"," "," "))
+print("|{0:-<27}|{0:-<27}|{0:-<27}|".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("Student",locale.currency(priceList[2]),locale.currency(priceList[7])))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("(Student ID)"," "," "))
+print("|{0:-<27}|{0:-<27}|{0:-<27}|".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("Senior",locale.currency(priceList[3]),locale.currency(priceList[8])))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("(Ages 60+)"," "," "))
+print("|{0:-<27}|{0:-<27}|{0:-<27}|".format("-"))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("Adult",locale.currency(priceList[4]),locale.currency(priceList[9])))
+print("|{0:<27}|{1:^27}|{2:^27}|".format("(Ages 18+)"," "," "))
+print("{0:-<28}{0:-<28}{0:-<29}\n".format("-"))
 #
-dateChoice = letInput("Please choose Peak or Off Peak\nEnter P for peak and O for off peak:- ","I only accept P or O, please try again")
+dateChar = letInput("Please choose Peak or Off Peak\nEnter P for peak and O for off peak:- ","OP","I only accept P or O, please try again")
+if dateChar == "O":
+    priceOffset = 0
+else:
+    priceOffset = 5
 #
 visitorCountList[0] = intInput("How many tickets for children do you want?- ","I only accept whole numbers here, please try again")
 visitorCountList[1] = intInput("How many tickets for teenagers do you want?- ","I only accept whole numbers here, please try again")
@@ -77,24 +80,35 @@ visitorCountList[3] = intInput("How many tickets for seniors do you want?- ","I 
 visitorCountList[4] = intInput("How many tickets for adults do you want?- ","I only accept whole numbers here, please try again")
 #
 if visitorCountList[2] == 1:
-    print("There is a student coming, please show your ID now?")
-    x = input("Press enter when done")
+    print("\nThere is a student coming, please show your ID now?")
 elif visitorCountList[2] == 2:
-    print("There are {0} students coming, please will both of you show ids now?".format(visitorCountList[2]))
-    x = input("Press enter when done")
+    print("\nThere are {0} students coming, please will both of you show ids now?".format(visitorCountList[2]))
 elif visitorCountList[2] >= 2:
-    print("There are {0} students coming, please will all {0} of you show ids now?".format(visitorCountList[2]))
-    x = input("Press enter when done")
+    print("\nThere are {0} students coming, please will all {0} of you show ids now?".format(visitorCountList[2]))
+idCount = 0
+if visitorCountList[2] > 0:
+    while 1:
+        idCount = intInput("How many valid IDs were shown? :-","I only accept Y or N here")
+        if idCount > visitorCountList[2]:
+            print("How can there be more IDs that there are students?  Try again!")
+        elif idCount < visitorCountList[2]:
+            notStudents = visitorCountList[2]-idCount
+            visitorCountList[2] = visitorCountList[2] - notStudents
+            visitorCountList[4] = visitorCountList[4] + notStudents
+            print("{} students failed to show valid Ids so I'm charging you as if they were adults.".format(notStudents))
+            break
+        else:
+            break
 #
 # Price calculations
 totalPrice = 0
 for x in range(0,5):
-    totalPrice=totalPrice+visitorCountList[x]*priceList[x+dateChoice]
+    totalPrice=totalPrice+visitorCountList[x]*priceList[x+priceOffset]
 #
 # Print out explanation and price
-if dateChoice == 0:
+if priceOffset == 0:
     print("\n\nYou chose an off peak day")
 else:
     print("\n\nYou chose a day in Peak time")
 print("and there are {} children, {} teens, {} students, {} seniors and {} adults coming\n".format(visitorCountList[0], visitorCountList[1], visitorCountList[2], visitorCountList[3], visitorCountList[4]))
-print("So the total price is £{:.2f}.  Thanks for your custom\n".format(totalPrice))
+print("So the total price is {}.  Thanks for your custom\n".format(locale.currency(totalPrice)))
