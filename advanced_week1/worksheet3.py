bba5f18fc9d4b4915e1b0ba53b601b088689edf5
Kim Nobay <kim@nobay.co.uk>
2018-02-18 13:00:13 +0000

    did the other temperature one

--- advanced_week1/worksheet3.py
+++ advanced_week1/worksheet3.py
@@ -5,10 +5,18 @@ def convert(fahrenheit):
     centigrade = (fahrenheit - 32) * (5 / 9)
     return centigrade
 
+def convert_to_fahrenheit(centigrade):
+    "convert centigrade to fahrenheit "
+    fahrenheit = (centigrade * (9 / 5)) + 32
+    return fahrenheit
+
 def main():
     "The main program"
     fahrenheit = float(input("Enter value in Fahrenheit"))
     print(convert(fahrenheit))
+
+    centigrade = float(input("Enter value in Centigrade"))
+    print(convert_to_fahrenheit(centigrade))
 
 if __name__ == "__main__":
     main()
