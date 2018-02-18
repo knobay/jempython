96906a3b07c6dfc0cb965c7d9c23deb96299386c
Kim Nobay <kim@nobay.co.uk>
2018-02-18 12:22:26 +0000

    did the basic function questions

---
+++ advanced_week1/worksheet2.py
@@ -0,0 +1,16 @@
+def printhello():
+    # Sub procedure to print my name
+    print('Hello World!')
+
+#main program
+
+def main():
+    "The main program"
+    for i in range(10):
+        printhello()
+        pass
+
+if __name__ == "__main__":
+    main()
+    
+
